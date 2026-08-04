bbdbe667d9c8502598b1d423f266cf8ac2c5777a
andres-coda <codaandres@gmail.com>
2026-08-04 08:37:20 -0300

    Incorpore nivel a la api

--- src/componente/componente.module.ts
+++ src/componente/componente.module.ts
@@ -2,9 +2,7 @@ import { forwardRef, Module } from '@nestjs/common';
 import { ComponenteController } from './componente.controller';
 import { ComponenteService } from './componente.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from '../user/entity/user.entity';
 import { Componente } from './entity/componente.entity';
-import { Libro } from '../libro/entity/libro.entity';
 import { AuthModule } from '../auth/auth.module';
 import { UserModule } from '../user/user.module';
 import { ErroresModule } from '../error/error.module';
@@ -13,9 +11,7 @@ import { GateWayModule } from '../gateway/gateway.module';
 @Module({
   imports: [
     TypeOrmModule.forFeature([
-      User,
       Componente,
-      Libro
     ]),
     forwardRef(() => AuthModule),
     forwardRef(() => UserModule),
